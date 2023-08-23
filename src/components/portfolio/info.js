4285bd1ce7c2205ef1d848dad5c96d9556363866
sheriff-gaye <sheriffgaye5@gmail.com>
2023-08-23 06:53:10 +0000

    all

--- src/components/portfolio/info.js
+++ src/components/portfolio/info.js
@@ -24,23 +24,23 @@ export const portfolio_data=[
         "demo_url": "https://sheriff-gaye.github.io/getfit",
         "git_url": "https://github.com/sheriff-gaye/getfit"
     },
-    {
-        "id":4,
-        "image": require("../../assets/p4.jpg"),
-        "title": "Portfolio Website",
-        "desc": "A simple website that showcases an individual's expertise, skills, and the products or services they give to the public.",
-        "demo_url": "https://sheriff-gaye.github.io/javascript_projects/portfolio/index.html",
-        "git_url": "https://github.com/sheriff-gaye/javascript_projects/tree/main/portfolio"
-    },
-    {
-        "id":5,
-        "image": require("../../assets/notes.jpeg"),
-        "title": "Easy Notes",
-        "desc": "Easy Notes is a simple note-taking app written in React Js that stores data in local storage written in Javascript and has a minimalistic user interface for adding, editing, and deleting notes.",
-        "demo_url": "https://sheriff-gaye.github.io/note_app/",
-        "git_url": "https://github.com/sheriff-gaye/note_app"
-    }
-    ,
+    // {
+    //     "id":4,
+    //     "image": require("../../assets/p4.jpg"),
+    //     "title": "Portfolio Website",
+    //     "desc": "A simple website that showcases an individual's expertise, skills, and the products or services they give to the public.",
+    //     "demo_url": "https://sheriff-gaye.github.io/javascript_projects/portfolio/index.html",
+    //     "git_url": "https://github.com/sheriff-gaye/javascript_projects/tree/main/portfolio"
+    // },
+    // {
+    //     "id":5,
+    //     "image": require("../../assets/notes.jpeg"),
+    //     "title": "Easy Notes",
+    //     "desc": "Easy Notes is a simple note-taking app written in React Js that stores data in local storage written in Javascript and has a minimalistic user interface for adding, editing, and deleting notes.",
+    //     "demo_url": "https://sheriff-gaye.github.io/note_app/",
+    //     "git_url": "https://github.com/sheriff-gaye/note_app"
+    // }
+    // ,
     {
         "id":6,
         "image": require("../../assets/natures2.jpeg"),
@@ -49,22 +49,22 @@ export const portfolio_data=[
         "demo_url": "https://naturesyoghurt.com",
         "git_url": ""
     },
-    {
-        "id":7,
-        "image": require("../../assets/toggle.png"),
-        "title": "Livewire Toggle Switch",
-        "desc": "Laravel Livewire Toggle Switch enables creating a dynamic database app with a simple on/off switch functionality, providing a seamless user experience for managing data.",
-        "demo_url": "",
-        "git_url": "https://github.com/sheriff-gaye/ToggleDatabase"
-    },
-    {
-        "id":8,
-        "image": require("../../assets/Screenshot 2023-05-28 at 10.29.20 PM.png"),
-        "title": "Livewire Export Data to (CSV, PDF, XLS)",
-        "desc": "Laravel Livewire Export Data App: Effortlessly generate CSV, PDF, and XLS files from your Laravel application using the powerful Livewire framework.",
-        "demo_url": "",
-        "git_url": "https://github.com/sheriff-gaye/exportDataLivewire"
-    },
+    // {
+    //     "id":7,
+    //     "image": require("../../assets/toggle.png"),
+    //     "title": "Livewire Toggle Switch",
+    //     "desc": "Laravel Livewire Toggle Switch enables creating a dynamic database app with a simple on/off switch functionality, providing a seamless user experience for managing data.",
+    //     "demo_url": "",
+    //     "git_url": "https://github.com/sheriff-gaye/ToggleDatabase"
+    // },
+    // {
+    //     "id":8,
+    //     "image": require("../../assets/Screenshot 2023-05-28 at 10.29.20 PM.png"),
+    //     "title": "Livewire Export Data to (CSV, PDF, XLS)",
+    //     "desc": "Laravel Livewire Export Data App: Effortlessly generate CSV, PDF, and XLS files from your Laravel application using the powerful Livewire framework.",
+    //     "demo_url": "",
+    //     "git_url": "https://github.com/sheriff-gaye/exportDataLivewire"
+    // },
     {
         "id":9,
         "image": require("../../assets/client.png"),
